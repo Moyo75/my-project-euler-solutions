229be586d4e02c471e8ed6181b5b8468019814e5
John Ademoye <moyojohn188@gmail.com>
2020-01-07 07:46:08 +0100

    Did multiplesof3and5

---
+++ 1.MultiplesOf3And5.js
@@ -0,0 +1,15 @@
+function multiplesOf3and5(number) {
+  // Good luck!
+  let sum = 0;
+
+  for(let i = 1; i < number; i++) {
+    if(i % 3 === 0 || i % 5 === 0)
+      {
+        sum = sum + i;
+      }
+  }
+  
+  return sum;
+}
+
+//multiplesOf3and5(1000);
